c9508be6cec2fe4aca72feb8f72a2cb575697549
Ryan Brainard <brainard@heroku.com>
2012-03-20 15:22:03 -0700

    remove heroku host param until warpath supports it

--- src/main/java/com/heroku/HerokuDeployer.java
+++ src/main/java/com/heroku/HerokuDeployer.java
@@ -20,28 +20,13 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Sample {@link Builder}.
- * <p/>
- * <p/>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link HerokuDeployer} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #artifactPath})
- * to remember the configuration.
- * <p/>
- * <p/>
- * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
- * method will be invoked.
- *
- * @author Kohsuke Kawaguchi
+ * @author Ryan Brainard
  */
 public class HerokuDeployer extends Builder {
 
     private final String appName;
     private final String artifactPath;
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
     public HerokuDeployer(String appName, String artifactPath) {
         this.appName = appName;
@@ -52,9 +37,6 @@ public class HerokuDeployer extends Builder {
         return appName;
     }
 
-    /**
-     * We'll use this from the <tt>config.jelly</tt>.
-     */
     public String getArtifactPath() {
         return artifactPath;
     }
@@ -100,41 +82,21 @@ public class HerokuDeployer extends Builder {
         return (DescriptorImpl) super.getDescriptor();
     }
 
-    /**
-     * Descriptor for {@link HerokuDeployer}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     * <p/>
-     * <p/>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HerokuDeployer/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-        /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
-         * <p/>
-         * <p/>
-         * If you don't want fields to be persisted, use <tt>transient</tt>.
-         */
-        private String herokuHost = "heroku.com";
+
         private String apiKey;
 
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
             return true;
         }
 
-        /**
-         * This human readable name is used in the configuration screen.
-         */
         public String getDisplayName() {
             return "Deploy to Heroku";
         }
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            herokuHost = formData.getString("herokuHost");
             apiKey = formData.getString("apiKey");
 
             save();
@@ -143,10 +105,6 @@ public class HerokuDeployer extends Builder {
 
         public String getApiKey() {
             return apiKey;
-        }
-
-        public String getHerokuHost() {
-            return herokuHost;
         }
     }
 }
