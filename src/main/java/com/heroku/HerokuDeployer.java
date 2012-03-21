f13847437081bcde7e6026a55f8a9736c786258b
Ryan Brainard <brainard@heroku.com>
2012-03-20 17:58:16 -0700

    allow local api key to override the default

--- src/main/java/com/heroku/HerokuDeployer.java
+++ src/main/java/com/heroku/HerokuDeployer.java
@@ -8,6 +8,7 @@ import com.herokuapp.warpath.WarPusher;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
@@ -29,11 +30,13 @@ import java.io.IOException;
  */
 public class HerokuDeployer extends Builder {
 
+    private final String apiKey;
     private final String appName;
     private final String artifactPath;
 
     @DataBoundConstructor
-    public HerokuDeployer(String appName, String artifactPath) {
+    public HerokuDeployer(String apiKey, String appName, String artifactPath) {
+        this.apiKey = apiKey;
         this.appName = appName;
         this.artifactPath = artifactPath;
     }
@@ -46,10 +49,27 @@ public class HerokuDeployer extends Builder {
         return artifactPath;
     }
 
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    private String getEffectiveApiKey() {
+        if (apiKey != null && !apiKey.trim().equals("")) {
+            return apiKey;
+        }
+
+        final String defaultApiKey = getDescriptor().getDefaultApiKey();
+        if (defaultApiKey != null && !defaultApiKey.trim().equals("")) {
+            return defaultApiKey;
+        }
+
+        throw new RuntimeException("Heroku API key not specified.");
+    }
+
     @Override
     public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener) {
-        final String apiKey = getDescriptor().getApiKey(); //TODO: error handle
-        final HerokuAPI api = new HerokuAPI(apiKey);
+        final String effectiveApiKey = getEffectiveApiKey();
+        final HerokuAPI api = new HerokuAPI(effectiveApiKey);
 
         App app;
         try {
@@ -70,7 +90,7 @@ public class HerokuDeployer extends Builder {
         try {
             build.getWorkspace().child(artifactPath).act(new FilePath.FileCallable<Void>() {
                 public Void invoke(File artifactFile, VirtualChannel channel) throws IOException, InterruptedException {
-                    new WarPusher(apiKey).push(appName, artifactFile);
+                    new WarPusher(effectiveApiKey).push(appName, artifactFile);
                     return null;
                 }
             });
@@ -94,7 +114,7 @@ public class HerokuDeployer extends Builder {
     @Extension
     public static final class HerokuDeployerDescriptor extends BuildStepDescriptor<Builder> {
 
-        private String apiKey;
+        private String defaultApiKey;
 
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
             return true;
@@ -106,26 +126,46 @@ public class HerokuDeployer extends Builder {
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            apiKey = formData.getString("apiKey");
+            defaultApiKey = formData.getString("defaultApiKey");
 
             save();
             return super.configure(req, formData);
         }
 
-        public String getApiKey() {
-            return apiKey;
+        public String getDefaultApiKey() {
+            return defaultApiKey;
         }
 
-        public FormValidation doCheckAppName(@QueryParameter String value) {
-            return FormValidation.validateRequired(value);
-        }
-
-        public FormValidation doCheckArtifactPath(@AncestorInPath AbstractProject project, @QueryParameter String value) throws IOException {
-            if (value != null && !value.trim().endsWith(".war")) {
-                return FormValidation.error("Artifact must be a WAR file");
+        public FormValidation doCheckApiKey(@QueryParameter String apiKey) {
+            if (Util.fixEmptyAndTrim(apiKey) != null && Util.fixEmptyAndTrim(defaultApiKey) != null) {
+                return FormValidation.warning("This key will override the default key. Set to blank to use default key.");
             }
 
-            return FilePath.validateFileMask(project.getSomeWorkspace(), value);
+            if (Util.fixEmptyAndTrim(apiKey) != null) {
+                return FormValidation.ok();
+            }
+
+            if (Util.fixEmptyAndTrim(defaultApiKey) != null) {
+                return FormValidation.ok("Default API key will be used.");
+            }
+
+            return FormValidation.validateRequired(apiKey);
+        }
+
+        public FormValidation doCheckAppName(@QueryParameter String appName) {
+            return FormValidation.validateRequired(appName);
+        }
+
+        public FormValidation doCheckArtifactPath(@AncestorInPath AbstractProject project, @QueryParameter String artifactPath) throws IOException {
+            if (Util.fixEmptyAndTrim(artifactPath) == null) {
+                return FormValidation.validateRequired(artifactPath);
+            }
+
+            if (!artifactPath.endsWith(".war")) {
+                return FormValidation.error("Must be a WAR file");
+            }
+
+            return FilePath.validateFileMask(project.getSomeWorkspace(), artifactPath);
         }
     }
 }
