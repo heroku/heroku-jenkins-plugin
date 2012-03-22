1698f8c7fa4dc9cdb658b14d68aa7953d7da9ed1
Ryan Brainard <brainard@heroku.com>
2012-03-21 17:58:29 -0700

    add rollback command

---
+++ src/main/java/com/heroku/Rollback.java
@@ -0,0 +1,62 @@
+package com.heroku;
+
+import com.heroku.api.App;
+import com.heroku.api.HerokuAPI;
+import com.heroku.api.Release;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.util.FormValidation;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Ryan Brainard
+ */
+public class Rollback extends AbstractHerokuBuildStep {
+
+    @DataBoundConstructor
+    public Rollback(String apiKey, String appName, String command) {
+        super(apiKey, appName);
+    }
+
+    @Override
+    protected boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener, HerokuAPI api, App app) throws IOException, InterruptedException {
+        final List<Release> releases = api.listReleases(app.getName());
+        final Release lastRelease = releases.get(releases.size() - 2);
+
+        listener.getLogger().println("Rolling back to " + lastRelease.getName() + "(" + lastRelease.getCommit() + ") ...");
+        api.rollback(app.getName(), lastRelease.getName());
+        listener.getLogger().println("Rollback complete");
+
+        return true;
+    }
+
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl) super.getDescriptor();
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends AbstractHerokuBuildStepDescriptor {
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        public String getDisplayName() {
+            return "Heroku: Rollback";
+        }
+
+        public FormValidation doCheckCommand(@AncestorInPath AbstractProject project, @QueryParameter String command) throws IOException {
+            return FormValidation.validateRequired(command);
+        }
+    }
+}
