ccd796f56165b1bfa5e30ca29d7f5dd9e41590c3
Ryan Brainard <brainard@heroku.com>
2012-04-05 16:48:41 -0700

    basic run process test

---
+++ src/test/java/com/heroku/RunProcessTest.java
@@ -0,0 +1,21 @@
+package com.heroku;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author Ryan Brainard
+ */
+public class RunProcessTest extends BaseHerokuBuildStepTest {
+
+    public void testRunProcess() throws Exception {
+        FreeStyleProject project = createFreeStyleProject();
+        project.getBuildersList().add(new RunProcess(apiKey, appName, "pwd"));
+
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+
+        String logs = FileUtils.readFileToString(build.getLogFile());
+        assertTrue("Should have connected to dyno and run `pwd`", logs.contains("/app"));
+    }
+}
