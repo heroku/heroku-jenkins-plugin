a732266f2acd2af2f53e4a3eb85df1bbdd4c91d3
Ryan Brainard <brainard@heroku.com>
2012-05-15 13:02:08 -0700

    Use env vars in tests as fallback if sys prop is not set

--- src/test/java/com/heroku/BaseHerokuBuildStepTest.java
+++ src/test/java/com/heroku/BaseHerokuBuildStepTest.java
@@ -15,8 +15,8 @@ import static org.mockito.Mockito.when;
  */
 public abstract class BaseHerokuBuildStepTest extends HudsonTestCase {
 
-    protected final String appName = System.getProperty("heroku.appName");
-    protected final String apiKey = System.getProperty("heroku.apiKey");
+    protected final String appName = getSystemPropertyOrEnvVar("heroku.appName", "HEROKU_APP_NAME");
+    protected final String apiKey = getSystemPropertyOrEnvVar("heroku.apiKey", "HEROKU_API_KEY");
 
     protected final HerokuAPI api = new HerokuAPI(apiKey);
     protected final BuildListener listener = mock(BuildListener.class);
@@ -43,5 +43,16 @@ public abstract class BaseHerokuBuildStepTest extends HudsonTestCase {
 
     protected interface AppRunnable {
         void run(App app) throws Exception;
+    }
+    
+    private static String getSystemPropertyOrEnvVar(String systemProperty, String envVar) {
+        if (System.getProperties().containsKey(systemProperty)) {
+            return  System.getProperty(systemProperty);
+        } else if (System.getenv().containsKey(envVar)) {
+            return System.getenv(envVar);
+        } else {
+            throw new RuntimeException("Could not log system property [" + systemProperty + "]" +
+                                       " or environment variable [" + envVar + "]");
+        }
     }
 }
