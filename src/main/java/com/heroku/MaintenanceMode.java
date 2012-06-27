9d340877be23b4e434761acd353d303b8611b7f5
Ryan Brainard <brainard@heroku.com>
2012-06-26 23:14:15 -0700

    add maintenance mode task

---
+++ src/main/java/com/heroku/MaintenanceMode.java
@@ -0,0 +1,71 @@
+package com.heroku;
+
+import com.heroku.api.App;
+import com.heroku.api.HerokuAPI;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.util.ListBoxModel;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+
+/**
+ * @author Ryan Brainard
+ */
+public class MaintenanceMode extends AbstractHerokuBuildStep {
+
+    private final boolean mode;
+
+    @DataBoundConstructor
+    public MaintenanceMode(String apiKey, String appName, boolean mode) {
+        super(apiKey, appName);
+        this.mode = mode;
+    }
+
+    // Overriding and delegating to parent because Jelly only looks at concrete class when rendering views
+    @Override
+    public String getAppName() {
+        return super.getAppName();
+    }
+
+    // Overriding and delegating to parent because Jelly only looks at concrete class when rendering views
+    @Override
+    public String getApiKey() {
+        return super.getApiKey();
+    }
+
+    public boolean getMode() {
+        return mode;
+    }
+
+    @Override
+    protected boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener, HerokuAPI api, App app) throws IOException, InterruptedException {
+        listener.getLogger().println((mode ? "Enabling" : "Disabling") + " maintenance mode for app " + app.getName());
+        api.setMaintenanceMode(app.getName(), mode);
+        return true;
+    }
+
+
+    @Override
+    public MaintenanceModeDescriptor getDescriptor() {
+        return (MaintenanceModeDescriptor) super.getDescriptor();
+    }
+
+    @Extension
+    public static final class MaintenanceModeDescriptor extends AbstractHerokuBuildStepDescriptor {
+
+        public String getDisplayName() {
+            return "Heroku: Maintenance Mode";
+        }
+
+        public ListBoxModel doFillModeItems() {
+            ListBoxModel items = new ListBoxModel();
+            items.add("Enabled", Boolean.TRUE.toString());
+            items.add("Disabled", Boolean.FALSE.toString());
+            return items;
+        }
+
+    }
+}
---
+++ src/test/java/com/heroku/MaintenanceModeTest.java
@@ -0,0 +1,29 @@
+package com.heroku;
+
+import com.heroku.api.App;
+import hudson.model.FreeStyleProject;
+
+/**
+ * @author Ryan Brainard
+ */
+public class MaintenanceModeTest extends BaseHerokuBuildStepTest {
+
+    public void testToggleMaintenanceMode() throws Exception {
+        final FreeStyleProject project = createFreeStyleProject();
+
+        runWithNewApp(new AppRunnable() {
+            public void run(App app) throws Exception {
+                String appName = app.getName();
+
+                project.getBuildersList().add(new MaintenanceMode(apiKey, appName, true));
+                project.scheduleBuild2(0).get();
+                assertTrue(api.isMaintenanceModeEnabled(appName));
+
+                project.getBuildersList().clear();
+                project.getBuildersList().add(new MaintenanceMode(apiKey, appName, false));
+                project.scheduleBuild2(0).get();
+                assertFalse(api.isMaintenanceModeEnabled(appName));
+            }
+        });
+    }
+}
