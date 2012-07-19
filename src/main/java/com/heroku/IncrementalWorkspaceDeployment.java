d4923b0c37255f2228068b4c733d40464298467a
Ryan Brainard <brainard@heroku.com>
2012-07-19 16:25:40 -0700

    strip down incremental workspace deploy to only use null buildpack and hide build output

--- src/main/java/com/heroku/IncrementalWorkspaceDeployment.java
+++ src/main/java/com/heroku/IncrementalWorkspaceDeployment.java
@@ -2,20 +2,27 @@ package com.heroku;
 
 import com.heroku.api.App;
 import com.heroku.api.HerokuAPI;
+import com.herokuapp.janvil.Config;
+import com.herokuapp.janvil.EventSubscription;
+import com.herokuapp.janvil.Janvil;
+import com.herokuapp.janvil.Manifest;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
 import hudson.util.DirScanner;
+import hudson.util.FileVisitor;
 import hudson.util.FormValidation;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Collections;
 
 /**
  * @author Ryan Brainard
@@ -56,13 +63,73 @@ public class IncrementalWorkspaceDeployment extends AbstractHerokuBuildStep {
 
     @Override
     public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener, final HerokuAPI api, final App app) throws IOException, InterruptedException {
-        return build.getWorkspace().act(new AnvilDeployer(
-                listener,
-                getEffectiveApiKey(),
-                app,
-                new DirScanner.Glob(globIncludes, globExcludes),
-                NULL_BUILDPACK,
-                new HashMap<String, String>()));
+        final String userAgent = new JenkinsUserAgentValueProvider().getLocalUserAgent();
+
+        return build.getWorkspace().act(new FilePath.FileCallable<Boolean>() {
+
+            final boolean[] slugPushed = new boolean[]{false}; //TODO: use exit code
+
+            public Boolean invoke(File baseDir, VirtualChannel channel) throws IOException, InterruptedException {
+                final Janvil janvil = new Janvil(
+                        new Config(getEffectiveApiKey())
+                                .setConsumersUserAgent(userAgent)
+                                .setReadMetadata(false)
+                                .setWriteMetadata(false)
+                                .setEventSubscription(new EventSubscription<Janvil.Event>(Janvil.Event.class)
+                                        .subscribe(Janvil.Event.DIFF_START, new EventSubscription.Subscriber<Janvil.Event>() {
+                                            public void handle(Janvil.Event event, Object numTotalFiles) {
+                                                listener.getLogger().println("Workspace contains " + numTotalFiles + " files...");
+                                            }
+                                        })
+                                        .subscribe(Janvil.Event.UPLOADS_START, new EventSubscription.Subscriber<Janvil.Event>() {
+                                            public void handle(Janvil.Event event, Object numDiffFiles) {
+                                                listener.getLogger().println("Uploading " + numDiffFiles + " new files...");
+                                            }
+                                        })
+                                        .subscribe(Janvil.Event.BUILD_OUTPUT_LINE, new EventSubscription.Subscriber<Janvil.Event>() {
+                                            public void handle(Janvil.Event event, Object data) {
+                                                // do not output to jenkins user
+                                                System.out.println(data); //TODO: REMOVE
+
+                                                slugPushed[0] |= (String.valueOf(data).contains("Success, slug is ")); //TODO: use exit code
+                                            }
+                                        })
+                                        .subscribe(Janvil.Event.RELEASE_START, new EventSubscription.Subscriber<Janvil.Event>() {
+                                            public void handle(Janvil.Event event, Object data) {
+                                                listener.getLogger().println("Deploying...");
+                                            }
+                                        })
+                                        .subscribe(Janvil.Event.RELEASE_END, new EventSubscription.Subscriber<Janvil.Event>() {
+                                            public void handle(Janvil.Event event, Object versionNumber) {
+                                                listener.getLogger().println("Released " + versionNumber + " to " + app.getWebUrl());
+                                            }
+                                        })));
+
+                final Manifest manifest = new Manifest(baseDir);
+                new DirScanner.Glob(globIncludes, globExcludes).scan(baseDir, new FileVisitor() {
+                    @Override
+                    public void visit(File f, String relativePath) throws IOException {
+                        if (f.isFile()) {
+                            manifest.add(f);
+                        }
+                    }
+                });
+
+                slugPushed[0] = false; //TODO: use exit code
+
+                janvil.build(manifest, Collections.<String, String>emptyMap(), NULL_BUILDPACK);
+
+                //TODO: use exit code
+                if (!slugPushed[0]) {
+                    listener.error("Upload failed. Aborting deployment.");
+                    return false;
+                }
+
+                janvil.release(app.getName(), manifest);
+
+                return true;
+            }
+        });
     }
 
     @Override
